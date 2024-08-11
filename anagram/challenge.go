eb898a49468d095d206db6697e97cb03789e1d65
Kyron Stubbs <kstubbs04@gmail.com>
2024-08-11 19:35:35 +0000

    log output

--- anagram/challenge.go
+++ anagram/challenge.go
@@ -1,9 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+func solultion1(w1 string, w2 string) bool {
+
+	howLong := len(w1)
+	thisLong := len(w2)
+
+	//The
+	if howLong != thisLong {
+		return false
+	}
+
+	return false
+}
 
 func main() {
-	fmt.Println("this is the end")
+
+	w1 := "fireds"
+	w2 := "fried"
+
+	if solultion1(w1, w2) {
+		println("%s and %s are anagrams of each other", w2, w2)
+	} else {
+		println("%s and %s are not anagrams of each other", w2, w2)
+	}
+
 }
