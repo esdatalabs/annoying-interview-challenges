68efd86a840a7eeaa5e6003fc2a17bd237345667
Kyron Stubbs <kstubbs04@gmail.com>
2024-08-11 21:29:53 +0000

    update test; changed methods names for clarity

--- anagram/challenge.go
+++ anagram/challenge.go
@@ -2,29 +2,58 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"slices"
 	"strings"
 )
 
-func solultion1(w1 string, w2 string) bool {
+func anagrams(w1 string, w2 string) bool {
 
-	howLong := len(w1)
-	thisLong := len(w2)
+	//Convert both string to arrays of 32bit integers (specifically unicode points)
+	//Because having a character type like every other grown ass language would go against the grain
+	//of golang core competencies
+	w1r := []rune(w1)
+	w2r := []rune(w2)
 
-	if howLong != thisLong {
+	for _, entry := range w1r {
+		if !slices.Contains(w2r, entry) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func validate(w1 string, w2 string) bool {
+
+	regex := regexp.MustCompile(`^[a-zA-Z]+$`)
+	var lettersOnly = regex.MatchString
+
+	//If the words are the same, they're not anagrams
+	if w1 == w2 {
 		return false
 	}
 
-	return false
+	//words must be of same length
+	if len(w1) != len(w2) {
+		return false
+	}
+
+	//words must be composed of letters only
+	if !lettersOnly(w1) || !lettersOnly(w2) {
+		return false
+	}
+
+	return true
 }
 
-func validate(w1 string, w2 string) {
-
-}
-
+// Ensure we're comparing apples to apples
 func normalize(word string) string {
 
+	//Purse all spaces
 	normalized := strings.ReplaceAll(word, " ", "")
 
+	//make everything lowercase
 	normalized = strings.ToLower(normalized)
 
 	return normalized
@@ -32,15 +61,20 @@ func normalize(word string) string {
 
 func main() {
 
-	w1 := "fireds"
+	w1 := "fired"
 	w2 := "fried"
 
-	if solultion1(w1, w2) {
+	w1n := normalize(w1)
+	w2n := normalize(w2)
+
+	eligible := validate(w1n, w2n)
+
+	if eligible && anagrams(w1, w2) {
 		fmt.Printf("%s and %s are anagrams of each other", w1, w2)
 	} else {
 		fmt.Printf("%s and %s are not anagrams of each other", w1, w2)
 	}
-
+	println("")
 	println("end")
 
 }
--- anagram/challenge_test.go
+++ anagram/challenge_test.go
@@ -24,3 +24,28 @@ func Test_normalize(t *testing.T) {
 	}
 
 }
+
+type allowed struct {
+	wrd1, wrd2 string
+	valid      bool
+}
+
+var suite2 = []allowed{
+	{"thisismixedspace", "thisismixedspace", false},
+	//Words must be of the same length
+	{"givenLength", "short", false},
+	//Words must be alphabetical
+	{"&es_", "word", false},
+	//Valid word pairs
+	{"fired", "fried", true},
+}
+
+func Test_eligibleWordPairs(t *testing.T) {
+
+	for _, entry := range suite2 {
+		if response := validate(entry.wrd1, entry.wrd2); response != entry.valid {
+			t.Errorf("Expected %v for validating %s and %s. Got %v", entry.valid, entry.wrd1, entry.wrd2, response)
+		}
+	}
+
+}
