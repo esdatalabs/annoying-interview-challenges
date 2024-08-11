a1be730412b1679d60aae06d3a754dcc0150682a
Kyron Stubbs <kstubbs04@gmail.com>
2024-08-11 22:01:58 +0000

    normalize and validate methods are the same

--- palindrome/challenge.go
+++ palindrome/challenge.go
@@ -1,11 +1,45 @@
 package main
 
 import (
-	"fmt"
-
-	"rsc.io/quote"
+	"regexp"
+	"strings"
 )
 
+func validate(w1 string, w2 string) bool {
+
+	regex := regexp.MustCompile(`^[a-zA-Z]+$`)
+	var lettersOnly = regex.MatchString
+
+	//If the words are the same, they're not anagrams
+	if w1 == w2 {
+		return false
+	}
+
+	//words must be of same length
+	if len(w1) != len(w2) {
+		return false
+	}
+
+	//words must be composed of letters only
+	if !lettersOnly(w1) || !lettersOnly(w2) {
+		return false
+	}
+
+	return true
+}
+
+// Ensure we're comparing apples to apples
+func normalize(word string) string {
+
+	//Purse all spaces
+	normalized := strings.ReplaceAll(word, " ", "")
+
+	//make everything lowercase
+	normalized = strings.ToLower(normalized)
+
+	return normalized
+}
+
 func main() {
-	fmt.Println(quote.Go())
+
 }
